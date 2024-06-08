61b0b90b4a0caeb6f9ef02fc93224e307f4267b7
dikiharyadi19 <dk.harryadi@gmail.com>
2024-06-08 12:21:27 +0700

    feat: adding router

--- router/router.go
+++ router/router.go
@@ -9,49 +9,69 @@ import (
 	"strings"
 	"time"
 
+	"github.com/diki-haryadi/govega/monitor"
+	"github.com/diki-haryadi/govega/response"
 	"github.com/felixge/httpsnoop"
 	"github.com/julienschmidt/httprouter"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 
-	"github.com/opentracing/opentracing-go"
-	tlog "github.com/opentracing/opentracing-go/log"
-
-	"gitlab.com/superman-tech/lib/log"
-	"gitlab.com/superman-tech/lib/monitor"
-	"gitlab.com/superman-tech/lib/response"
-	customHttpUtil "gitlab.com/superman-tech/lib/util"
+	"github.com/diki-haryadi/govega/log"
 )
 
-type MyRouter struct {
-	Httprouter     *httprouter.Router
-	WrappedHandler http.Handler
-	Options        *Options
-}
+type (
+	MyRouter struct {
+		Httprouter     *httprouter.Router
+		WrappedHandler http.Handler
+		Options        *Options
+		tracer         trace.Tracer
+	}
 
-type Options struct {
-	Prefix  string
-	Timeout int
-}
+	Options struct {
+		Prefix  string
+		Timeout int
+	}
+
+	captureConfig struct {
+		captureHandler bool
+	}
+
+	httpParamsKey     struct{}
+	responseWriterKey struct{}
+	captureHandlerKey struct{}
+)
 
 var (
-	HttpRouter *httprouter.Router
+	// list of paths which we don't want to capture the data
+	nonCapturePaths = []string{
+		"/metrics",
+		"/health",
+		"/debug/pprof",
+	}
+
+	httpResponseLogKey = attribute.Key("http.response.log")
 )
 
-func init() {
-	HttpRouter = httprouter.New()
+func GetHttpParam(ctx context.Context, name string) string {
+	ps := ctx.Value(httpParamsKey{}).(httprouter.Params)
+	return ps.ByName(name)
 }
 
-func WrapperHandler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		m := httpsnoop.CaptureMetrics(HttpRouter, w, r)
-		if r.URL.String() != "/metrics" && !strings.Contains(r.URL.String(), "/healthz") { // Note: Dont want to monitor for /metrics and /healthz
-			monitor.FeedHTTPMetrics(m.Code, m.Duration, r.Header.Get("routePath"), r.Method)
-		}
-	})
+func GetResponseWriter(ctx context.Context) http.ResponseWriter {
+	val := ctx.Value(responseWriterKey{})
+	if val == nil {
+		return nil
+	}
+	return val.(http.ResponseWriter)
 }
 
 func New(o *Options) *MyRouter {
-	myrouter := &MyRouter{Options: o}
-	myrouter.Httprouter = HttpRouter
+	myrouter := &MyRouter{
+		Options: o,
+		tracer:  otel.Tracer("router/myrouter"),
+	}
+	myrouter.Httprouter = httprouter.New()
 	return myrouter
 }
 
@@ -78,9 +98,11 @@ func (mr *MyRouter) DELETE(path string, handle Handle) {
 }
 
 func (mr *MyRouter) Handle(path, method string, handle Handle) {
-	fullPath := mr.Options.Prefix + path
-	log.Println(fullPath)
-	mr.Httprouter.Handle(method, fullPath, mr.handleNow(fullPath, handle))
+	mr.registerHandle(path, method, handleToHttpRouterHandle(handle))
+}
+
+func (mr *MyRouter) Handler(path, method string, handler http.Handler) {
+	mr.registerHandle(path, method, httpHandlerToHttpRouterHandle(handler))
 }
 
 func (mr *MyRouter) ServeFiles(path string, root http.FileSystem) {
@@ -89,55 +111,93 @@ func (mr *MyRouter) ServeFiles(path string, root http.FileSystem) {
 }
 
 func (mr *MyRouter) Group(path string, fn func(r *MyRouter)) {
-	sr := New(&Options{
-		Prefix:  mr.Options.Prefix + path,
-		Timeout: mr.Options.Timeout,
-	})
+	sr := &MyRouter{
+		Options: &Options{
+			Prefix:  mr.Options.Prefix + path,
+			Timeout: mr.Options.Timeout,
+		},
+		Httprouter: mr.Httprouter,
+		tracer:     mr.tracer,
+	}
 	fn(sr)
 }
 
-type panicObject struct {
-	err        interface{}
-	stackTrace string
+// Deprecated: use MyRouter instead, it's already implement http.Handler
+func (mr *MyRouter) HttpHandler() http.Handler {
+	return mr
 }
 
-func (mr *MyRouter) handleNow(fullPath string, handle Handle) httprouter.Handle {
+func (mr *MyRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	captureConf := captureConfig{
+		captureHandler: true,
+	}
+	ctx := context.WithValue(r.Context(), captureHandlerKey{}, &captureConf)
+
+	m := httpsnoop.CaptureMetrics(mr.Httprouter, w, r.WithContext(ctx))
+
+	if captureConf.captureHandler {
+		monitor.FeedHTTPMetrics(m.Code, m.Duration, r.Header.Get("routePath"), r.Method)
+	}
+}
+
+func (mr *MyRouter) registerHandle(path, method string, handle httprouter.Handle) {
+	fullPath := mr.Options.Prefix + path
+	log.Println(fullPath)
+	mr.Httprouter.Handle(method, fullPath, mr.handlePath(fullPath, handle))
+}
+
+func (mr *MyRouter) handlePath(fullPath string, handle httprouter.Handle) httprouter.Handle {
+	var captureHandler = true
+	for _, path := range nonCapturePaths {
+		if strings.Contains(fullPath, path) {
+			captureHandler = false
+			break
+		}
+	}
+
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		var (
-			span opentracing.Span
-			ctx  context.Context
-		)
-
-		t := time.Now()
 		ctx, cancel := context.WithTimeout(r.Context(), time.Second*time.Duration(mr.Options.Timeout))
-
 		defer cancel()
 
-		ctx = context.WithValue(ctx, "HTTPParams", ps)
-		ctx = context.WithValue(ctx, "ResponseWriter", w)
+		ctx = context.WithValue(ctx, httpParamsKey{}, ps)
 
-		if !strings.Contains(fullPath, "/healthz") {
-			span, ctx = opentracing.StartSpanFromContext(ctx, r.RequestURI)
-			defer span.Finish()
-			span.LogFields(tlog.String("ip", customHttpUtil.GetClientIPAddress(r)))
+		if value, ok := ctx.Value(captureHandlerKey{}).(*captureConfig); ok {
+			value.captureHandler = captureHandler
 		}
 
 		r.Header.Set("routePath", fullPath)
+
 		r = r.WithContext(ctx)
 
+		handle(w, r, ps)
+	}
+}
+
+func httpHandlerToHttpRouterHandle(handler http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		ctx := r.Context()
+
+		ctx = context.WithValue(ctx, httpParamsKey{}, ps)
+
+		panicHttpHandlerWrapper(handler).ServeHTTP(w, r.WithContext(ctx))
+	}
+}
+
+func handleToHttpRouterHandle(handle Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		t := time.Now()
+
+		ctx := r.Context()
+
+		ctx = context.WithValue(ctx, httpParamsKey{}, ps)
+		ctx = context.WithValue(ctx, responseWriterKey{}, w)
+
 		respChan := make(chan *response.JSONResponse)
-		recovered := make(chan panicObject)
+
+		r = r.WithContext(ctx)
 
 		go func() {
-			defer func() {
-				if err := recover(); err != nil {
-					recovered <- panicObject{
-						err:        err,
-						stackTrace: string(debug.Stack()),
-					}
-				}
-			}()
-			respChan <- handle(r)
+			respChan <- panicHandleWrapper(handle)(r)
 		}()
 
 		select {
@@ -145,22 +205,13 @@ func (mr *MyRouter) handleNow(fullPath string, handle Handle) httprouter.Handle 
 			if ctx.Err() == context.DeadlineExceeded {
 				response.NewJSONResponse().SetError(response.ErrTimeoutError).Send(w)
 			}
-		case cause := <-recovered:
-			httpDump := dumpRequest(r)
-			log.WithFields(log.Fields{
-				"path":       r.URL.Path,
-				"httpDump":   string(httpDump),
-				"stackTrace": cause.stackTrace,
-				"error":      fmt.Sprintf("%v", cause.err),
-			}).Errorln("[Router] panic have occurred")
-			response.NewJSONResponse().SetError(response.ErrInternalServerError).Send(w)
 		case resp := <-respChan:
 			if resp != nil {
-				if span != nil {
-					span.LogFields(tlog.Object("log", resp.Log))
-					span.SetTag("httpCode", resp.StatusCode)
-				}
+				span := trace.SpanFromContext(ctx)
+				span.SetAttributes(httpResponseLogKey.String(fmt.Sprintf("%+v", resp.Log)))
+
 				resp.SetLatency(time.Since(t).Seconds() * 1000)
+
 				if resp.StatusCode > 499 {
 					m := map[string]interface{}{}
 					httpDump := dumpRequest(r)
@@ -169,32 +220,66 @@ func (mr *MyRouter) handleNow(fullPath string, handle Handle) httprouter.Handle 
 					m["DUMP:"] = string(httpDump)
 					log.Printf("%+v", m)
 				}
+
 				resp.Send(w)
 			} else {
-				if fullPath != "/metrics" {
-					httpDump := dumpRequest(r)
-					log.WithFields(log.Fields{
-						"dump": string(httpDump),
-					}).Errorln("[Router] Nil response received from the handler")
-					response.NewJSONResponse().SetError(response.ErrInternalServerError).Send(w)
-				}
+				httpDump := dumpRequest(r)
+
+				log.WithFields(log.Fields{
+					"dump": string(httpDump),
+				}).Errorln("[Router] Nil response received from the handler")
+
+				response.NewJSONResponse().SetError(response.ErrInternalServerError).Send(w)
 			}
 		}
+	}
+}
+
+func panicHttpHandlerWrapper(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				httpDump := dumpRequest(r)
+				stackTrace := string(debug.Stack())
+
+				log.WithFields(log.Fields{
+					"path":       r.URL.Path,
+					"httpDump":   string(httpDump),
+					"stackTrace": stackTrace,
+					"error":      fmt.Sprintf("%+v", err),
+				}).Errorln("[router/panicHttpHandlerWrapper] panic have occurred")
+
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				return
+			}
+		}()
+
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func panicHandleWrapper(handle Handle) Handle {
+	return func(r *http.Request) (resp *response.JSONResponse) {
+		defer func() {
+			if err := recover(); err != nil {
+				httpDump := dumpRequest(r)
+				stackTrace := string(debug.Stack())
+
+				log.WithFields(log.Fields{
+					"path":       r.URL.Path,
+					"httpDump":   string(httpDump),
+					"stackTrace": stackTrace,
+					"error":      fmt.Sprintf("%+v", err),
+				}).Errorln("[router/panicHandleWrapper] panic have occurred")
+
+				resp = response.NewJSONResponse().SetError(response.ErrInternalServerError)
+				return
+			}
+		}()
+
+		resp = handle(r)
 		return
 	}
-}
-
-func GetHttpParam(ctx context.Context, name string) string {
-	ps := ctx.Value("HTTPParams").(httprouter.Params)
-	return ps.ByName(name)
-}
-
-func GetResponseWriter(ctx context.Context) http.ResponseWriter {
-	val := ctx.Value("ResponseWriter")
-	if val == nil {
-		return nil
-	}
-	return val.(http.ResponseWriter)
 }
 
 func dumpRequest(r *http.Request) []byte {
@@ -202,17 +287,20 @@ func dumpRequest(r *http.Request) []byte {
 	if err == nil {
 		return httpDump
 	}
+
 	log.WithFields(log.Fields{
 		"url":    r.URL,
 		"method": r.Method,
 		"header": fmt.Sprintf("%+v", r.Header),
 		"err":    err,
 	}).Debugln("[Router] Failed to dump request with body, re-attempting to dump request without body")
-	//Retry without including body
+
+	// Retry without including body
 	httpDump, err = httputil.DumpRequest(r, false)
 	if err == nil {
 		return httpDump
 	}
+
 	log.WithFields(log.Fields{
 		"url":    r.URL,
 		"method": r.Method,
